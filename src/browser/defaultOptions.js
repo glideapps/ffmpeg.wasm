200bfd52f69c3f2ecf716cdf99f31da91c11ae98
Jeremy Llewellyn <jeremy.llewellyn@heyglide.com>
2023-06-05 12:40:04 -0700

    Add missing import with updated repackage name

--- src/browser/defaultOptions.js
+++ src/browser/defaultOptions.js
@@ -3,8 +3,6 @@ import * as resolveURL from 'resolve-url';
 /*
  * Default options for browser environment
  */
-const corePath = typeof process !== 'undefined' && process.env.NODE_ENV === 'development'
-  ? resolveURL('/node_modules/@ffmpeg/core/dist/ffmpeg-core.js')
-  : `https://unpkg.com/@ffmpeg/core@${pkg.devDependencies['@ffmpeg/core'].substring(1)}/dist/ffmpeg-core.js`;
+const corePath = resolveURL('/node_modules/@ffmpeg/core/dist/ffmpeg-core.js');
 
 export default { corePath };
